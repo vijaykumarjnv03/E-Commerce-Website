c8fe3bd0ae244d3451415f92c0b0960f978130c6
Vijay <vijaykumarjnv03@gmail.com>
2021-12-31 14:35:38 +0530

    fronted is done, time to start the redux

---
+++ frontend/src/component/layout/Footer/Footer.js
@@ -0,0 +1,34 @@
+import React from "react";
+import playStore from "../../../images/playstore.png";
+import appStore from "../../../images/Appstore.png";
+import "./Footer.css"
+
+const Footer = () => {
+    return (
+        <footer id = "footer">
+            <div class="leftFooter">
+                <h4>DOWNLOAD OUR APP</h4>
+                <p>Download App for Android and IOS mobile phone</p>
+                <img src={playStore} alt="playstore" />
+                <img src={appStore} alt="Appstore" />
+            </div>
+ 
+            <div class="midFooter">
+                <h1>easyGo</h1>
+                <p>High Quality is our first priority</p>
+                <p>Copyrights 2022 &copy; Vijay Kumar</p>
+                
+            </div>
+
+            <div class="rightFooter">
+                <h4>Follow Us</h4>
+                <a href="https://www.instagram.com/its.vijay007/">Instagram</a>
+                <a href="https://www.youtube.com/channel/UCQHV6ZHtER_xm3BXIqsdMHg">Youtube</a>
+                <a href="https://www.facebook.com/vijaykumarjnv03/">Facebook</a>
+            </div>
+        </footer>
+
+    );
+};
+
+export default Footer;
