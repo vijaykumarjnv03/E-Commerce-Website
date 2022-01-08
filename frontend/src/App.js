be22eb8fa4fb6a5bb371b4fdf3bf37a1891a6ae9
Vijay <vijaykumarjnv03@gmail.com>
2022-01-08 16:47:07 +0530

    filter products

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,8 @@ import React from "react";
 import Footer from "./component/layout/Footer/Footer.js"
 import Home from "./component/Home/Home.js";
 import ProductDetails from "./component/Product/ProductDetails.js"
-
-
+import Products from "./component/Product/Products.js"
+import Search from "./component/Product/Search.js"
 
 
 function App() {
@@ -25,7 +25,10 @@ function App() {
       <Header />
       <Route exact path="/" component={Home}/>
       <Route exact path="/product/:id" component={ProductDetails}/>
-
+      <Route exact path="/products" component={Products}/>
+      <Route path="/products/:keyword" component={Products} />
+      
+      <Route exact path="/search" component={Search} />
 
       <Footer />
     </Router>
--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -9,23 +9,32 @@ import {
     CLEAR_ERRORS 
 } from "../constants/productConstants";
 
-export const getProduct = () => async (dispatch) => {
+// Get All Products
+export const getProduct =
+  (keyword = "", currentPage = 1, price = [0, 25000], category) =>
+  async (dispatch) => {
     try {
-        dispatch({ type: ALL_PRODUCT_REQUEST });
+      dispatch({ type: ALL_PRODUCT_REQUEST });
 
-        const {data} = await axios.get("/api/v1/products");
+      let link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}`;
 
-        dispatch({
-            type:ALL_PRODUCT_SUCCESS,
-            payload: data,
-        })
-    }catch(error){
-        dispatch({
-            type: ALL_PRODUCT_FAIL,
-            payload: error.response.data.message,
-        });
+      if (category) {
+        link = `/api/v1/products?keyword=${keyword}&page=${currentPage}&price[gte]=${price[0]}&price[lte]=${price[1]}&category=${category}`;
+      }
+
+      const { data } = await axios.get(link);
+
+      dispatch({
+        type: ALL_PRODUCT_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      dispatch({
+        type: ALL_PRODUCT_FAIL,
+        payload: error.response.data.message,
+      });
     }
-};
+  };
 
 export const getProductDetails = (id) => async (dispatch) => {
     try {
